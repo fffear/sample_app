ca7638a0c09c38455ab00b0f6896c694bedded6e
Kevin Vuong <c_k.k@hotmail.com>
2019-07-10 20:16:31 +1000

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,7 +5,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
-  test "layout links" do
+  def setup
+    @user = users(:michael)
+  end
+
+  test "layout links for non-logged-in users" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
@@ -13,9 +17,27 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign Up")
+  end
+
+  test "layout links for logged-in users" do
+    get login_path
+    assert_template 'sessions/new'
+    log_in_as(@user)
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", edit_user_path
+    assert_select "a[href=?]", user_path
   end
 end
